bb0b81ff3369e1d461b94eb5998da7fccecd4461
calebcheptumo <ckibusia@gmail.com>
2023-12-21 13:20:32 +0300

    Variables

---
+++ variables-data-types/variables.py
@@ -0,0 +1,18 @@
+#what is a variable? A variable is a container for a value, which can be of various types.
+#Variable named message and assigned a string value
+#Variable names are case sensitive
+#Variable names cannot start with a number
+#Variable names cannot contain spaces
+#Variable names cannot contain special characters
+#Variable names should be descriptive and meaningful
+#Variable names should be in lower case and words separated by underscore
+#Variable names should not be python keywords
+#Variable names should not be built-in functions
+#Variable assignment is done using the assignment operator (=)
+#Variable can hold different types of values such as string, integer, float, boolean, list, tuple, dictionary, etc.
+message = "Hello Variable"
+print(message)
+
+
+
+
