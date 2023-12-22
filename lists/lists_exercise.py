c5087df6182456b2c712790b1a12b1cc354647d1
calebcheptumo <ckibusia@gmail.com>
2023-12-22 11:52:33 +0300

    Lists exercise

--- lists/lists_exercise.py
+++ lists/lists_exercise.py
@@ -11,3 +11,51 @@ print(f"Hello, beautiful {my_friends[2]} i have missed you so much.")
 
 music = ["Rnb" ,"AfroBeat" ,"Reggae" ,"Hip-pop" ,"Gospel"] 
 print(f"My favorite {music[1]} song is YAWA by Fireboy DML")
+
+
+dinner_friends = ["Joseph", "Glenn", "Sally"]
+print(f"Hi, {dinner_friends[0]} would you like to come over for dinner ?")
+print(f"Hi, {dinner_friends[1]} there is a new movie out, would you like to come over and watch it ?")
+print(f"Hi beautiful {dinner_friends[2]} i have missed you so much, would you like to come over for dinner ?")
+
+dinner_friends = ["Joseph", "Glenn", "Sally"]
+not_coming = dinner_friends.pop(0)
+print(f"Unfortunately {not_coming} wont be able to make to dinner.")
+dinner_friends.insert(0 ,"Maggy")
+print(dinner_friends)
+print(f"Hi, {dinner_friends[0]} i would like to invite you to dinner.")
+print(f"Hi, {dinner_friends[1]} i would like to invite you to dinner.")
+print(f"Hi {dinner_friends[2]} i would like to invite you to dinner.")
+
+
+print(dinner_friends)
+dinner_friends.insert( 0 ,"Juliet")
+dinner_friends.insert(2,"Vivian")
+dinner_friends.append("Linda")
+print(dinner_friends)
+
+print(f"Hi, {dinner_friends[0]} i would like to invite you to dinner.")
+print(f"Hi, {dinner_friends[1]} i would like to invite you to dinner.")
+print(f"Hi, {dinner_friends[2]} i would like to invite you to dinner.")
+print(f"Hi, {dinner_friends[3]} i would like to invite you to dinner.")
+print(f"Hi, {dinner_friends[4]} i would like to invite you to dinner.")
+print(f"Hi, {dinner_friends[5]} i would like to invite you to dinner.")
+
+
+print(dinner_friends)
+print("You can only invite two people for dinner")
+not_inviting = dinner_friends.pop()
+print(f"Sorry {not_inviting} i wont be able to invite you ")
+not_inviting = dinner_friends.pop()
+print(f"Sorry {not_inviting} i wont be able to invite you")
+not_inviting = dinner_friends.pop()
+print(f"Sorry {not_inviting} i wont be able to invite you")
+not_inviting = dinner_friends.pop()
+print(f"Sorry {not_inviting} i wont be able to invite you")
+print(dinner_friends)
+print(f"Hi {dinner_friends[0]} you are still invited to dinner")
+print(f"Hi {dinner_friends[1]} you are still invited to dinner")
+
+print(dinner_friends)
+del dinner_friends[-2] #deleting the last 2 items in the list
+print(dinner_friends)
