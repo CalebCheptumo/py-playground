5b8d06fe2d66dd736a804df5c15de6c77e7f88a4
calebcheptumo <ckibusia@gmail.com>
2023-12-22 09:57:33 +0300

    Lists exercise

---
+++ lists/lists_exercise.py
@@ -0,0 +1,13 @@
+my_friends = ["Joseph", "Glenn", "Sally"]
+print(my_friends[0])
+print(my_friends[1])
+print(my_friends[2])
+
+
+print(f"Hi, {my_friends[0]} how are you doing ?")
+print(f"Wozaa, {my_friends[1]} i was wondering if you could help me with something ?")
+print(f"Hello, beautiful {my_friends[2]} i have missed you so much.")
+
+
+music = ["Rnb" ,"AfroBeat" ,"Reggae" ,"Hip-pop" ,"Gospel"] 
+print(f"My favorite {music[1]} song is YAWA by Fireboy DML")
