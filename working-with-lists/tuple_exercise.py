b46cc0ba69e64550902b5d0d59cae214bd175d2b
calebcheptumo <ckibusia@gmail.com>
2023-12-26 19:36:08 +0300

    Tuple exercise

---
+++ working-with-lists/tuple_exercise.py
@@ -0,0 +1,7 @@
+foods = ("Fish", "Pillau", "Fries", "Chicken", "Tea")
+for food in foods:
+    print(food)
+
+foods = ("Fish", "Pillau", "Fries", "Ugali", "Liver")
+for food in foods:
+    print(food)
