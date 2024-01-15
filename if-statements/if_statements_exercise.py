3893713599bba42f1d606ff37112a5b985f08b8c
calebcheptumo <ckibusia@gmail.com>
2024-01-15 08:54:08 +0300

    If statement exercise

---
+++ if-statements/if_statements_exercise.py
@@ -0,0 +1,34 @@
+# Conditional tests
+month = 'January'
+print("Is month == 'January'? True.")
+print("month == 'January'")
+
+print("\nIs month == 'February'? False")
+print("month == 'February'")
+
+
+print("Test 1: Is 'python' == 'python'? I predict True.")
+print('python' == 'python')
+
+print("\nTest 2: Is 'python' != 'java'? I predict True.")
+print('python' != 'java')
+
+
+print("\nTest 3: Is 'Python'.lower() == 'python'? I predict True.")
+print('Python'.lower() == 'python')
+
+print("\nTest 4: Is 'JAVA'.lower() == 'java'? I predict True.")
+print('JAVA'.lower() == 'java')
+
+languages = ['python', 'java', 'c++', 'javascript']
+print("\nTest 13: Is 'python' in languages? I predict True.")
+print('python' in languages)
+
+print("\nTest 14: Is 'ruby' in languages? I predict False.")
+print('ruby' in languages)
+
+print("\nTest 15: Is 'ruby' not in languages? I predict True.")
+print('ruby' not in languages)
+
+print("\nTest 16: Is 'python' not in languages? I predict False.")
+print('python' not in languages)
