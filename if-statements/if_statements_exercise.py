2db622d7441e943c56ceb4e717891864cfed0835
calebcheptumo <ckibusia@gmail.com>
2024-01-15 10:26:18 +0300

    If statement exercise

--- if-statements/if_statements_exercise.py
+++ if-statements/if_statements_exercise.py
@@ -32,3 +32,57 @@ print('ruby' not in languages)
 
 print("\nTest 16: Is 'python' not in languages? I predict False.")
 print('python' not in languages)
+
+
+# alien colors
+alien_color = 'green'
+if alien_color == 'green':
+    print("You have earned 5 points ")
+if alien_color == 'red':
+    print("You have 0earned 0 point")
+
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("You earned 50 points")
+else:
+    print("You earned 10 point")
+
+
+alien_color = 'green'
+if alien_color == 'green':
+    print("You earned 5 points")
+elif alien_color == 'yellow':
+    print("You earned 10 points")
+elif alien_color == 'red':
+    print("You earned 15 points")
+
+age = 18
+if age < 2:
+    print("Baby")
+elif age < 4:
+    print("Toddler")
+elif age < 13:
+    print("Kid")
+elif age < 20:
+    print("Teenager")
+elif age < 65:
+    print("Adult")
+elif age >= 65:
+    print("Elder")
+
+
+favorite_fruits = ['mango','apple','pineapple','watermellon']
+if 'mango' in favorite_fruits:
+    print("i like mango")
+if 'orange' in favorite_fruits:
+    print("i really like orange")
+if 'apple' in favorite_fruits:
+    print("i like apple")
+if 'pineapple' in favorite_fruits:
+    print("i like pineapple")
+if 'avocado' in favorite_fruits:
+    print("i like avocado")
+if 'watermellon' in favorite_fruits:
+    print("i like watermellon")
+
