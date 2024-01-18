8fedf4840707e632302205631a13e99fe1309bdb
calebcheptumo <ckibusia@gmail.com>
2024-01-18 09:42:28 +0300

    Favorite number - exercise

--- dictionaries/dictionaries_exercise.py
+++ dictionaries/dictionaries_exercise.py
@@ -9,3 +9,25 @@ print(f"First name: {first_name}")
 print(f"Last name: {last_name}")
 print(f"Age: {age}")
 print(f"City: {city}")
+
+
+# Favorite number
+favorite_numbers = {
+    'caleb': 9,
+    'jane': 7,
+    'james': 3,
+    'josh': 5,
+    'jacob': 2
+}
+
+caleb = favorite_numbers['caleb']
+jane = favorite_numbers['jane']
+james = favorite_numbers['james']
+josh = favorite_numbers['josh']
+jacob = favorite_numbers['jacob']
+
+print(f"Caleb's favorite number is {caleb}")
+print(f"Jane's favorite number is {jane}")
+print(f"James's favorite number is {james}")
+print(f"Josh's favorite number is {josh}")
+print(f"Jacob's favorite number is {jacob}")
