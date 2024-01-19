1f5514c01327492e8e81b16df5f7107ae62168e5
calebcheptumo <ckibusia@gmail.com>
2024-01-19 14:13:40 +0300

    Using if statement

--- dictionaries/dictionaries.py
+++ dictionaries/dictionaries.py
@@ -202,3 +202,23 @@ for alien in aliens[:5]:  # slice the first 5 aliens
     print(alien)
 # show how many aliens have been created.
 print(f"Total number of aliens: {len(aliens)}")
+
+
+# use for loop and if statement o change color of aliens.
+
+# empty list to store aliens
+aliens = []  # empty list
+# make 30 green aliens
+for alien_number in range(30):
+    new_alien = {'color': 'green', 'points': 5, 'speed': 'slow'}
+    aliens.append(new_alien)
+
+for alien in aliens[:3]:
+    if alien['color'] == 'green':
+        alien['color'] = 'yellow'
+        alien['speed'] = 'medium'
+        alien['points'] = 10
+
+# show the first 5 aliens
+for alien in aliens[:5]:
+    print(alien)
