a53c6bd75dec20b5f8ccf7bf7ce34e9c3f62a577
calebcheptumo <ckibusia@gmail.com>
2024-01-20 21:49:26 +0300

    Restaurant seating exercise

--- user-input-and-while-loops/input_exercise.py
+++ user-input-and-while-loops/input_exercise.py
@@ -1,3 +1,13 @@
 # Rental car
 car = input("What type of car would you like to rent? ")
 print(f"Let me see if i can find you a {car}")
+
+
+# Restaurant seating
+seats = input("How many people are in their dinner group? ")
+seats = int(seats)
+
+if seats > 8:
+    print("You will have to wait for a table")
+else:
+    print("Your table is ready")
