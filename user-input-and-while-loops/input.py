0aecd2482373c04f00b2fb6b9e9a0a1ba45f8d70
calebcheptumo <ckibusia@gmail.com>
2024-01-20 21:22:41 +0300

    Prompt formating

--- user-input-and-while-loops/input.py
+++ user-input-and-while-loops/input.py
@@ -11,3 +11,15 @@ print(f"hello {name}")
 
 message = input("Tell me something, and I will repeat it back to you: ")
 print(message)
+
+# use easy and simple prompts
+# add a space at the end of the prompt to make it easier for the user to provide input
+# multiline prompt - use \n to add a new line in the prompt
+
+prompt = "If you tell us who you are, we can personalize the messages you see."
+# +=  operator takes the string that was stored in prompt and adds the new string onto the end
+prompt += "\nWhat is your first name? "
+
+
+name = input(prompt)
+print(f"Hello, {name}!")
