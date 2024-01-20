730c11d25a9d08f73b25f5201e726a40882baa26
calebcheptumo <ckibusia@gmail.com>
2024-01-20 21:55:51 +0300

    Multiple of 10 exercise

--- user-input-and-while-loops/input_exercise.py
+++ user-input-and-while-loops/input_exercise.py
@@ -11,3 +11,14 @@ if seats > 8:
     print("You will have to wait for a table")
 else:
     print("Your table is ready")
+
+
+# Multiples of ten
+number = input("Enter a number, and ill tell you if it even or odd: ")
+
+number = int(number)
+
+if number % 10 == 0:
+    print(f"{number} is a multiple of 10")
+else:
+    print(f"{number} is not a multiple of 10")
