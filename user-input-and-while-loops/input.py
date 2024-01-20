68b40c75df6e2cba087bfa191ff59fa5652135f0
calebcheptumo <ckibusia@gmail.com>
2024-01-20 21:13:31 +0300

    input def ,syntax

---
+++ user-input-and-while-loops/input.py
@@ -0,0 +1,10 @@
+# input() -function - prompts user to enter input
+# input() -function - returns string value
+# int() -function - converts string to integer
+# float() -function - converts string to float
+# str() -function - converts integer to string
+# str() -function - converts float to string
+# syntax: variable = input("prompt")
+
+name = input("Enter your name: ")
+print(f"hello {name}")
