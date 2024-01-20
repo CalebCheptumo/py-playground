35db3bf7d58e5acd1d50a58e54852517908d0eb4
calebcheptumo <ckibusia@gmail.com>
2024-01-20 22:01:27 +0300

    While loop example

--- user-input-and-while-loops/while_loops.py
+++ user-input-and-while-loops/while_loops.py
@@ -1,3 +1,9 @@
 # A while loop in Python repeatedly executes a target statement as long as a given condition is true.
 # syntax: while expression:
 #           statement(s)
+
+# Example 1: Print i as long as i is less than 6:
+i = 1
+while i < 6:
+    print(i)
+    i += 1
