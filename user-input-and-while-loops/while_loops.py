f83943ca5397e531bf5d9788ba656a9e339223af
calebcheptumo <ckibusia@gmail.com>
2024-01-23 22:14:20 +0300

    Count 1 to 5 while loop

--- user-input-and-while-loops/while_loops.py
+++ user-input-and-while-loops/while_loops.py
@@ -6,4 +6,12 @@
 i = 1
 while i < 6:
     print(i)
-    i += 1
+    i += 1 # i = i + 1
+
+
+
+#count form 1 to 5
+current_number = 1 # start at 1
+while current_number <= 5: # while loops as long as i is less than or equal to 5
+    print(current_number)
+    current_number +- 1
