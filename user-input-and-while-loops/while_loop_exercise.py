9e3622076633b49cd3f01cc0572aebbf2744d69c
calebcheptumo <ckibusia@gmail.com>
2024-01-28 22:25:12 +0300

    Using active variable exercise

--- user-input-and-while-loops/while_loop_exercise.py
+++ user-input-and-while-loops/while_loop_exercise.py
@@ -26,3 +26,24 @@ while True:
         print("Your ticket is $10")
     else:
         print("Your ticket is $15")
+
+
+# Three Exits
+prompt = "How old are you?"
+prompt += "\nEnter 'quit' when you are finished: "
+
+active = True
+while active:
+    age = input(prompt)
+    
+    if age == 'quit':
+        active = False
+        break
+
+    age = int(age)
+    if age < 3:
+        print("Your ticket is free")
+    elif age <= 12:
+        print("Your ticket is $10")
+    else:
+        print("Your ticket is $15")
