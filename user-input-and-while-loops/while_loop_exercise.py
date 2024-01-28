21c2310bd85806147a72eece062a77f7e9382e95
calebcheptumo <ckibusia@gmail.com>
2024-01-28 22:57:46 +0300

    No pastrami exercise

--- user-input-and-while-loops/while_loop_exercise.py
+++ user-input-and-while-loops/while_loop_exercise.py
@@ -71,3 +71,24 @@ print("\nThe following sandwiches have been made:")
 for sandwich in finished_sandwiches:
     print(sandwich)
 
+
+
+#no pastrami
+sandwich_orders = ['tuna', 'chicken', 'pastrami', 'beef', 'turkey', 'pastrami', 'veggie', 'pastrami']
+finished_sandwiches = []
+
+print("The deli has run out of pastrami.")
+
+while 'pastrami' in sandwich_orders:
+    sandwich_orders.remove('pastrami')
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)
+    print(f"I made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\nThe following sandwiches have been made:")
+for sandwich in finished_sandwiches:
+    print(sandwich)
+
+
