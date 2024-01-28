bf056f6dc6eb2fe1ff0f43909b28279526693cae
calebcheptumo <ckibusia@gmail.com>
2024-01-28 22:48:59 +0300

    Filling empty dictionary

--- user-input-and-while-loops/while_loops.py
+++ user-input-and-while-loops/while_loops.py
@@ -166,3 +166,26 @@ while 'cat' in pets: # while 'cat' is in pets
     pets.remove('cat') # remove 'cat' from pets
 
 print(pets)
+
+
+
+
+#filling a dictionary with user input
+
+responses = {} # empty dictionary to store responses
+
+# Set a flag to indicate that polling is active.
+polling_active = True
+
+while polling_active:
+    # Prompt for the person's name and response.
+    name = input("\nWhat is your name? ")
+    response = input("Which mountain would you like to climb someday? ")
+
+    # Store the response in the dictionary.
+    responses[name] = response
+
+    # Find out if anyone else is going to take the poll.
+    repeat = input("Would you like to let another person respond? (yes/ no) ")
+    if repeat == 'no':
+        polling_active = False
