7cf67dab820cd1b01ae74f115507d3efd295dcda
calebcheptumo <ckibusia@gmail.com>
2024-01-28 22:54:26 +0300

    Sandwiches exercise

--- user-input-and-while-loops/while_loop_exercise.py
+++ user-input-and-while-loops/while_loop_exercise.py
@@ -55,3 +55,19 @@ while active:
 number = 0
 while number < 5:
     print(number)
+
+
+
+#deli
+sandwich_orders = ['tuna', 'chicken', 'veggie', 'beef', 'turkey']
+finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop(0)
+    print(f"I made your {current_sandwich} sandwich.")
+    finished_sandwiches.append(current_sandwich)
+
+print("\nThe following sandwiches have been made:")
+for sandwich in finished_sandwiches:
+    print(sandwich)
+
