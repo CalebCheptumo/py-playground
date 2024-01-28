5421557c604c8500decd79cf2e6a8cc76953a279
calebcheptumo <ckibusia@gmail.com>
2024-01-28 22:22:54 +0300

    Movie tickets exercise

--- user-input-and-while-loops/while_loop_exercise.py
+++ user-input-and-while-loops/while_loop_exercise.py
@@ -7,3 +7,22 @@ while message != 'quit':
     message = input(prompt)
     if message != 'quit':
         print(f"You'll add {message} topping to your pizza!")
+
+
+# Movie Tickets
+prompt = "How old are you?"
+prompt += "\nEnter 'quit' when you are finished: "
+
+while True:
+    age = input(prompt)
+    
+    if age == 'quit':
+        break
+
+    age = int(age)
+    if age < 3:
+        print("Your ticket is free")
+    elif age <= 12:
+        print("Your ticket is $10")
+    else:
+        print("Your ticket is $15")
