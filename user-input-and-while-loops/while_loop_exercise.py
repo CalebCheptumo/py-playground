762b99d72aa7e5c18734a39a5813c28a1bb4d130
calebcheptumo <ckibusia@gmail.com>
2024-01-28 23:10:03 +0300

    Dream vacation exercise

--- user-input-and-while-loops/while_loop_exercise.py
+++ user-input-and-while-loops/while_loop_exercise.py
@@ -92,3 +92,22 @@ for sandwich in finished_sandwiches:
     print(sandwich)
 
 
+
+#dream vacation
+poll_results = {}
+
+while True:
+    name = input("What's your name? ")
+    place = input("If you could visit one place in the world, where would you go? ")
+
+    poll_results[name] = place
+
+    repeat = input("Would you like another person to vote? (yes/ no) ")
+    if repeat.lower() != 'yes':
+        break
+
+print("\n--- Poll Results ---")
+for name, place in poll_results.items():
+    print(f"{name.title()} would like to visit {place.title()}.")
+
+
