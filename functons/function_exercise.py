2c377cbf1ba6d05db09c43c1462e1f0663f96e2f
calebcheptumo <ckibusia@gmail.com>
2024-01-29 09:33:37 +0300

    Favorite book exercise

--- functons/function_exercise.py
+++ functons/function_exercise.py
@@ -5,3 +5,12 @@ def display_message():
     print("I am learning about functions in python")
 
 display_message()
+
+
+#favorite book
+
+def favorite_book(title):
+    """Display a message about someone's favorite book"""
+    print(f"My favorite book is {title.title()}")
+
+favorite_book('alice in wonderland')
