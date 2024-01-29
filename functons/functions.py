bd365922d481ca0036501b8b15f1e2792fd67f35
calebcheptumo <ckibusia@gmail.com>
2024-01-29 16:20:05 +0300

    Unmatched arguments error

--- functons/functions.py
+++ functons/functions.py
@@ -137,3 +137,14 @@ describe_pet(pet_name='nemo', animal_type='fish') #keyword arguments
 describe_pet(animal_type='fish', pet_name='nemo') #keyword arguments
 
 
+
+#avoiding argument errors
+#unmatched arguments occur when you provide fewer or more arguments than a function needs to do its work
+
+
+def describe_pet(animal_type,pet_name):
+    """Display information about a pet."""
+    print(f"\nI have a {animal_type}.")
+    print(f"My {animal_type}'s name is {pet_name.title()}.")
+
+describe_pet() #missing 2 required positional arguments: 'animal_type' and 'pet_name'
