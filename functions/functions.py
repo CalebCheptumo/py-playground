ef2541222b60e4a6300ba49ecb4102a74b048f84
calebcheptumo <ckibusia@gmail.com>
2024-01-30 12:01:59 +0300

    Function with while loop

--- functions/functions.py
+++ functions/functions.py
@@ -240,3 +240,21 @@ def build_person(first_name, last_name, age= None): #age is an optional paramete
 
 musician = build_person('jimi', 'hendrix', age=27)
 print(musician)
+
+
+
+#using a function with a while loop
+
+def get_formatted_name(first_name, last_name):
+    """Return a full name, neatly formatted"""
+    full_name = f"{first_name} {last_name}"
+    return full_name.title()
+
+#This is an infinite loop!
+while True:
+    print("\nPlease tell me your name:")
+    f_name = input("First name: ")
+    l_name = input("Last name: ")
+
+    formatted_name = get_formatted_name(f_name, l_name)
+    print(f"\nHello, {formatted_name}")
