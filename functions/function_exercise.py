723b84fb50874374ce0933518208a2ecba5ff075
calebcheptumo <ckibusia@gmail.com>
2024-01-30 13:03:23 +0300

    User album exercise

--- functions/function_exercise.py
+++ functions/function_exercise.py
@@ -90,3 +90,27 @@ print(album3)
 print(album4)
 
 
+#user albums
+def make_album(artist, title, tracks='None'):
+    """Return a dictionary of information about an album"""
+    album = {'artist': artist, 'title': title}
+    if tracks:
+        album['tracks'] = tracks
+    return album
+
+while True:
+    print("\nPlease enter the artist and album name:")
+    print("(enter 'q' at any time to quit)")
+
+    artist = input("Artist: ")
+    if artist == 'q':
+        break
+
+    title = input("Title: ")
+    if title == 'q':
+        break
+
+    album = make_album(artist, title)
+    print(album)
+
+
