6a4f89c52d29bcf00ad386d315bf0e5393e50208
calebcheptumo <ckibusia@gmail.com>
2024-01-30 21:35:30 +0300

    User profile exercise

--- functions/function_exercise.py
+++ functions/function_exercise.py
@@ -188,4 +188,17 @@ make_sandwich('cheese')
 make_sandwich('cheese', 'ham')
 make_sandwich('cheese', 'ham', 'lettuce')
 
+
+#user profile
+def build_profile(first, last, **user_info):
+    """Build a dictionary containing everything we know about a user"""
+    user_info['first_name'] = first
+    user_info['last_name'] = last
+    return user_info
+
+
+user_profile = build_profile('albert', 'einstein', location='princeton', field='physics')
+print(user_profile)
+
+
     
