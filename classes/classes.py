2bafa7162f60ef22c881e431ad7d40d7d8e15ba3
calebcheptumo <ckibusia@gmail.com>
2024-01-31 21:23:45 +0300

    Setting default value for an attribute

--- classes/classes.py
+++ classes/classes.py
@@ -132,3 +132,30 @@ class Car:
     
 my_new_car = Car('audi', 'a4', 2024)
 print(my_new_car.get_descriptive_name()) #call get_descriptive_name() method  to see what kind of car we have
+
+# setting a default value for an attribute
+#when an instance is created, attributes can be defined without being passed in as parameters. these attributes can be defined in the __init__() method, where they are assigned a default value.
+
+class Car:
+    """A simple attempt to represent a car. """
+
+    def __init__(self, make, model, year): #__init__() method takes in parameters to create an instance representing a particular car. when we make a new car, we'll need to specify a make, model, and year for it.
+        """Initialize attributes to describe a car. """
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = 0 #setting a default value for an attribute
+
+    def get_descriptive_name(self): # define a method called get_descriptive_name() that puts a car's year, make, and model into one string neatly describing the car.
+        """Return a neatly formatted descriptive name. """
+        long_name = f"{self.year} {self.make} {self.model}"
+        return long_name.title()    
+    
+    def read_odometer(self): #define a method called read_odometer() that lets us read the odometer
+        """Print a statement showing the car's mileage. """
+        print(f"This car has {self.odometer_reading} miles on it. ")
+
+
+my_new_car = Car('audi', 'a4', 2024)
+print(my_new_car.get_descriptive_name()) #call get_descriptive_name() method  to see what kind of car we have
+my_new_car.read_odometer() #call read_odometer() method to see the car's mileage
