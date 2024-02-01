22cb5ced4374251b73828a64c45b7d6ac7a55b26
calebcheptumo <ckibusia@gmail.com>
2024-02-01 22:23:31 +0300

    Number served exercise

--- classes/classes_exercise.py
+++ classes/classes_exercise.py
@@ -81,3 +81,41 @@ user2.greet_user()
 user3.describe_user()
 user3.greet_user()
 
+
+#number served
+class Restaurant:
+    def __init__(self, restaurant_name, cuisine_type):
+        """Initialize name and cuisine attributes"""
+        self.restaurant_name = restaurant_name
+        self.cuisine_type = cuisine_type
+        self.number_served = 0
+    
+    def describe_restaurant(self):
+        """Describe the restaurant"""
+        print(f"The restaurant's name is {self.restaurant_name.title()} .")
+        print(f"The restaurant's cuisine type is {self.cuisine_type.title()} .")
+
+    def open_restaurant(self):
+        """Open the restaurant"""
+        print(f"The restaurant {self.restaurant_name.title()} is now open. ")
+
+    def set_number_served(self, number_served):
+        """Set the number of customers served"""
+        self.number_served = number_served
+
+    def increment_number_served(self, additional_served):
+        """Increment the number of customers served"""
+        self.number_served += additional_served
+
+restaurant = Restaurant('kfc', 'fast food')
+print(restaurant.number_served)
+restaurant.number_served = 10
+print(restaurant.number_served)
+
+restaurant.set_number_served(20)
+print(restaurant.number_served)
+
+restaurant.increment_number_served(30)
+print(restaurant.number_served)
+
+
