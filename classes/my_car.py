f1bd75cc7bdb79ce8616a800547487066f85f735
calebcheptumo <ckibusia@gmail.com>
2024-02-01 23:57:00 +0300

    Importing entire module

--- classes/my_car.py
+++ classes/my_car.py
@@ -1,5 +1,5 @@
 from car import Car , ElectricCar #  import the Car class from the car module tells python to open the car.py file and copy all the classes from it into this program.
-
+import car # import the car module and then access the classes we need by using dot notation.
 my_new_car=Car('audi','a4',2019)
 print(my_new_car.get_descriptive_name())
 
@@ -12,4 +12,11 @@ my_tesla = ElectricCar('tesla','model s',2019)
 print(my_tesla.get_descriptive_name())
 
 my_toyota=Car('toyota','corolla',2019)
+print(my_toyota.get_descriptive_name())
+
+
+my_tesla = car.ElectricCar('tesla','model s',2019)
+print(my_tesla.get_descriptive_name())
+
+my_toyota = car.Car('toyota','corolla',2019)
 print(my_toyota.get_descriptive_name())
