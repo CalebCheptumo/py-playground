ac86b4255924321fda768a423cd60e72829b81b6
calebcheptumo <ckibusia@gmail.com>
2024-02-01 22:27:10 +0300

    Login Attempt exercise

--- classes/classes_exercise.py
+++ classes/classes_exercise.py
@@ -119,3 +119,42 @@ restaurant.increment_number_served(30)
 print(restaurant.number_served)
 
 
+#login attempts
+class User:
+    def __init__(self, first_name, last_name, age, location):
+        """Initialize name and age attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+        self.location = location
+        self.login_attempts = 0
+    
+    def describe_user(self):
+        """Describe the user"""
+        print(f"The user's name is {self.first_name.title()} {self.last_name.title()} .")
+        print(f"The user's age is {self.age} .")
+        print(f"The user's location is {self.location.title()} .")
+
+    def greet_user(self):
+        """Greet the user"""
+        print(f"Hello, {self.first_name.title()} {self.last_name.title()} .")
+
+    def increment_login_attempts(self):
+        """Increment the number of login attempts"""
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        """Reset the number of login attempts"""
+        self.login_attempts = 0
+
+user1 = User('John', 'Doe', 30, 'New York')
+
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+print(user1.login_attempts)
+
+user1.reset_login_attempts()
+print(user1.login_attempts)
