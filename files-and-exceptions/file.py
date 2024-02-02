d6815ddf1b08e20b27c8318db2265b8cafc74241
calebcheptumo <ckibusia@gmail.com>
2024-02-02 16:26:19 +0300

    Reading contents of a file

---
+++ files-and-exceptions/file.py
@@ -0,0 +1,8 @@
+#Reading from a file.
+#An incredible amount of data is available in text files. Text files can contain weather data, traffic data, socioeconomic data, literary works, and more. Reading from a file is particularly useful in data analysis applications, but it’s also applicable to any situation in which you want to analyze or modify information stored in a text file. For example, you can write a program that reads in the contents of a text file and rewrites the file with formatting that allows a browser to display it.
+
+#reading the context of a file
+#pi_digits.txt - contains pi to 30 decimal places with 10 decimal place per line
+#path is the exact location of a file or folder on a system.
+#python provides a module called pathlib that makes it easy to work with the file system.
+#a module that provides specific functionality like this is often called a library. hence, pathlib is a library.
---
+++ files-and-exceptions/file_reader.py
@@ -0,0 +1,6 @@
+#opens pi_digits.txt file , reads the contents of the file, and prints the contents to the screen.
+from pathlib import Path #import the Path class from the pathlib module
+
+path = Path("/home/caleb/Documents/CALEBTHEDEV/py-playground/files-and-exceptions/pi_digits.txt") #create a Path object that represents the location of the file on your system
+contents = path.read_text() #read the contents of the file and store the contents in a variable
+print(contents) #print the contents of the file to the screen
