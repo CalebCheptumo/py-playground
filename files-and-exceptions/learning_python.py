4425d9f9f9aeb01e562db7175e96183ba5c374bc
calebcheptumo <ckibusia@gmail.com>
2024-02-03 22:05:57 +0300

    Learning python exercise

---
+++ files-and-exceptions/learning_python.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+path = Path('files-and-exceptions/learning_python.txt') #using a relative path
+contents = path.read_text() #read the contents of the file
+print(contents) #print the contents of the file
+
+lines = contents.splitlines() #split the contents of the file into lines
+learning_string = '' #create an empty string to store the contents of the file
+
+for line in lines: #iterate over the lines in the file
+    learning_string += line.lstrip() #add the line to the learning_string, removing any leading whitespace
+
+print(learning_string) #print the learning_string   
