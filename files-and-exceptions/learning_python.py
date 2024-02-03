a9d9233423a5470ffe1ed3ee5d8a5d7a0819eabd
calebcheptumo <ckibusia@gmail.com>
2024-02-03 22:18:32 +0300

    Looping direcly over the list splitlines() returns

--- files-and-exceptions/learning_python.py
+++ files-and-exceptions/learning_python.py
@@ -4,10 +4,11 @@ path = Path('files-and-exceptions/learning_python.txt') #using a relative path
 contents = path.read_text() #read the contents of the file
 print(contents) #print the contents of the file
 
-lines = contents.splitlines() #split the contents of the file into lines
+#lines = contents.splitlines() #split the contents of the file into lines
 learning_string = '' #create an empty string to store the contents of the file
 
-for line in lines: #iterate over the lines in the file
+#for line in lines: #iterate over the lines in the file
+for line in contents.splitlines(): # you can loop directly over the list that splitlines() returns
     learning_string += line.lstrip() #add the line to the learning_string, removing any leading whitespace
 
 print(learning_string) #print the learning_string   
