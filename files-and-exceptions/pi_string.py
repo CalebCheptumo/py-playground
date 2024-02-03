4d253619aa0c9c6741bd96f145552d74c2fd032c
calebcheptumo <ckibusia@gmail.com>
2024-02-03 21:25:29 +0300

    Removing whitespace using lstrip()

--- files-and-exceptions/pi_string.py
+++ files-and-exceptions/pi_string.py
@@ -6,7 +6,8 @@ contents = path.read_text() #start by reading the entire contents of the file
 lines = contents.splitlines() # returns a list of all lines in the file the assign the list to variable lines
 pi_string = '' #initialize an empty string to hold the digits of pi
 for line in lines: #loop that adds line of digits to pi_string
-    pi_string += line #add each line to pi_string
+    #pi_string += line #add each line to pi_string
+    pi_string += line.lstrip() # remove leading whitespace from each line before adding it to pi_string
 
 
 print(pi_string) #print the string
