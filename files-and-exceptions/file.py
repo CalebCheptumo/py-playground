8646ee8180ecae66f416303ded41c6434f411e82
calebcheptumo <ckibusia@gmail.com>
2024-02-03 21:02:55 +0300

    Relative and absolute file paths

--- files-and-exceptions/file.py
+++ files-and-exceptions/file.py
@@ -6,3 +6,15 @@
 #path is the exact location of a file or folder on a system.
 #python provides a module called pathlib that makes it easy to work with the file system.
 #a module that provides specific functionality like this is often called a library. hence, pathlib is a library.
+
+
+
+#relative and absolute file paths
+#relative  file path tells python to look for a given location relative to the directory where the currently running program file is stored.
+#syntax: path = Path("relative_path_to_file")
+#example of relative path: path = Path("files-and-exceptions/pi_digits.txt")
+#absolute file path tells python exactly where a file is on your computer regardless of where the program that’s being executed is stored. it start from the root directory of the file system.
+#absolute path are usually longer than relative paths.
+#syntax: path = Path("absolute_path_to_file")
+#example of absolute path: path = Path("/home/caleb/Documents/CALEBTHEDEV/py-playground/files-and-exceptions/pi_digits.txt")
+#you can use either a relative or an absolute path to open a file. However, it’s important to be consistent in your use of these approaches. If you use relative paths, all the files you reference in your code will be in the same directory, or you’ll need to be aware of the directory structure of your project. If you use absolute paths, your code will only work on your computer, and it will break on anyone else’s computer. It’s also easier to move your project to another computer if you use relative paths. For this reason, we recommend using relative paths when possible.
