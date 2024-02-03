08bfd320cfac9688445d3eb1e6ae9fab7d30c193
calebcheptumo <ckibusia@gmail.com>
2024-02-03 23:25:25 +0300

    Exceptions

--- files-and-exceptions/exceptions.py
+++ files-and-exceptions/exceptions.py
@@ -1,1 +1,7 @@
 #Exceptions - a special Python class designed to held you respond to error s when they arise.
+#Python uses special objects called exceptions to manage errors that arise during a program's execution.
+#When an error occurs that makes Python unsure what to do next, it creates an exception object.
+#if you dont handle the exception, the program will halt and show a traceback, which includes a report of the exception that was raised.
+#Exceptions are handled with try-except blocks.
+#A try-except block asks Python to do something, but it also tells Python what to do if an exception is raised.
+#When you use try-except blocks, your programs will continue running even if things start to go wrong.
