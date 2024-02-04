2637366783b2157db7a9ff59c1fe11de80eeb66f
calebcheptumo <ckibusia@gmail.com>
2024-02-04 21:13:10 +0300

    count_words() - multiple files

---
+++ files-and-exceptions/word_count.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+def count_words(path): #define a function called count_words() that takes a path object as an argument.
+    """Count the approximate number of words in a file. """
+    #try- block to handle the exception that was raised.
+    try:
+        contents = path.read_text(encoding="utf-8")
+    except FileNotFoundError:
+        print(f"Sorry, the file {path} does not exist.")
+    else:
+        # Count the approximate number of words in the file:
+        words = contents.split() #split() method to produce a list of all the words in the file.
+        num_words = len(words) #use len() to estimate the number of words in the file.
+        print(f"The file {path} has about {num_words} words.")
+
+path = Path("files-and-exceptions/alice.txt") #create a path object that points to alice.txt
+count_words(path) #call the count_words() function and pass it the path object pointing to alice.txt.
