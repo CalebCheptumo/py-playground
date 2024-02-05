f687370373c42268f8aff81d8853127bd95ced09
calebcheptumo <ckibusia@gmail.com>
2024-02-05 22:36:48 +0300

    Combining retrive and store

--- files-and-exceptions/remember_me.py
+++ files-and-exceptions/remember_me.py
@@ -9,3 +9,17 @@ contents = json.dumps(username) #use the json.dumps() function to store the user
 path.write_text(contents) #write the contents to the file
 
 print(f"We'll remember you when you come back, {username}!") #print a message to the user
+
+
+#combine the two programs into one file
+
+path = Path('files-and-exceptions/username.json') #create a path object that points to username.json
+if path.exists():# exists() method returns True if a file exists and False if it does not.
+    contents = path.read_text() #read the contents of the file
+    username = json.loads(contents) #use the json.loads() function to read the username back into memory
+    print(f"Welcome back, {username}!") #greet the user whose name has already been stored in username.json
+else:
+    username = input("What is your name? ") #prompt the user for their name
+    contents = json.dumps(username) #use the json.dumps() function to store the username in the file username.json
+    path.write_text(contents) #write the contents to the file
+    print(f"We'll remember you when you come back, {username}!") #print a message to the user
