4e4a8516d4d726c82a71578949e812154b44c69e
calebcheptumo <ckibusia@gmail.com>
2024-02-06 21:20:08 +0300

    Prompt the user to see if their name is correct

--- files-and-exceptions/remember_me.py
+++ files-and-exceptions/remember_me.py
@@ -67,3 +67,36 @@ def greet_user():
         print(f"We'll remember you when you come back, {username}!")
 
 greet_user()
+
+
+
+#prompt the user to see if their name is correct
+def get_stored_username(path):
+    """Get stored username if available."""
+    if path.exists():
+        contents = path.read_text()
+        username = json.loads(contents)
+        return username
+    else:
+        return None
+    
+def get_new_username(path):
+    """Prompt for a new username."""
+    username = input("What is your name? ")
+    contents = json.dumps(username)
+    path.write_text(contents)
+    return username
+
+def greet_user():
+    """Greet the user by name."""
+    path = Path('files-and-exceptions/username.json')
+    username = get_stored_username(path)
+    if username:
+        correct = input(f"Are you {username}? (yes/no) ")
+        if correct == 'yes':
+            print(f"Welcome back, {username}!")
+        else:
+            username = get_new_username(path)
+            print(f"We'll remember you when you come back, {username}!")
+
+greet_user() #call the function greet_user() to greet the user by name
