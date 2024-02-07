c2b1de2a36314c21e97c5d476ff71f2eb24c158b
calebcheptumo <ckibusia@gmail.com>
2024-02-07 22:07:37 +0300

    Failed test exercise

--- testing-code/city-country-exercise/city_functions.py
+++ testing-code/city-country-exercise/city_functions.py
@@ -1,7 +1,7 @@
 #function that accepts two parameters: a city name and a country name. The function should return a single string of the form City, Country, such as Santiago, Chile.
 
 
-def get_formatted_name(city, country):
+def get_formatted_name(city, country, population):
     """Generate a neatly formatted city and country"""
-    formatted_name = f"{city}, {country}"
+    formatted_name = f"{city}, {country} - population {population}"
     return formatted_name.title()
