602dc373d6a364d7b11a3e96a88d209936553520
calebcheptumo <ckibusia@gmail.com>
2024-02-07 11:08:18 +0300

    Failing test

--- testing-code/name_function.py
+++ testing-code/name_function.py
@@ -1,4 +1,6 @@
-def get_formatted_name(first, last): # the function get_formatted_name() combines the first and last name
+#def get_formatted_name(first, last): # the function get_formatted_name() combines the first and last name
+def get_formatted_name(first, middle, last): # should work for people with middle names
     """Generate a neatly formatted full name"""
-    full_name = f"{first} {last}"
+    #full_name = f"{first} {last}"
+    full_name = f"{first} {middle} {last}"
     return full_name.title()
