9303225f0fc72a2d88ba96c6a0184991c8ff1c18
calebcheptumo <ckibusia@gmail.com>
2024-02-07 10:07:08 +0300

    Unit tests and test cases

--- testing-code/testing.py
+++ testing-code/testing.py
@@ -22,3 +22,13 @@
 
 
 #testing a function 
+
+
+#unit tests and test cases
+#unit test verifies that one specific aspect of a functions behavior is correct.
+#test case is a collection of unit tests that together prove that a function behaves as its supposed to, within the full range of situations you expect it to handle.
+#a good test case:
+#1. includes full range of unit tests for all the behavior you expect from a function.
+#2. includes tests to prove that a function fails in the ways you expect it to.
+#3. has complete coverage, meaning that it includes a full range of tests for all the code in a function.
+#4. is well-organized so you can understand the set of tests you've written.
