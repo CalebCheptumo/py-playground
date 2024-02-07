5940a2f0fc57ad18a319a8a47518331ef7e9e428
calebcheptumo <ckibusia@gmail.com>
2024-02-07 22:09:35 +0300

    Verify calling function with optional value

--- testing-code/city-country-exercise/city_functions.py
+++ testing-code/city-country-exercise/city_functions.py
@@ -1,7 +1,16 @@
 #function that accepts two parameters: a city name and a country name. The function should return a single string of the form City, Country, such as Santiago, Chile.
 
 
-def get_formatted_name(city, country, population):
+#def get_formatted_name(city, country, population):
+ #   """Generate a neatly formatted city and country"""
+  #  formatted_name = f"{city}, {country} - population {population}"
+   # return formatted_name.title()
+
+
+def get_formatted_name(city, country, population=0):
     """Generate a neatly formatted city and country"""
-    formatted_name = f"{city}, {country} - population {population}"
+    if population:
+        formatted_name = f"{city}, {country} - population {population}"
+    else:
+        formatted_name = f"{city}, {country}"
     return formatted_name.title()
--- testing-code/city-country-exercise/test_city_country.py
+++ testing-code/city-country-exercise/test_city_country.py
@@ -7,3 +7,9 @@ def test_city_country():
     """Do cities like 'santiago' and countries like 'chile' work?"""
     formatted_name = get_formatted_name('santiago', 'chile')
     assert formatted_name == 'Santiago, Chile'
+
+
+def test_city_country_population():
+    """Do cities like 'santiago' and countries like 'chile' work?"""
+    formatted_name = get_formatted_name('santiago', 'chile', 5000000)
+    assert formatted_name == 'Santiago, Chile - Population 5000000'
