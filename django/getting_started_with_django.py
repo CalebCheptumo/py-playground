3cc42a84f492069efbd54e92641dcef44c9b9027
calebcheptumo <ckibusia@gmail.com>
2024-02-08 21:37:43 +0300

    virtual environment

---
+++ django/getting_started_with_django.py
@@ -0,0 +1,8 @@
+#Django 
+#python most popular web framework , a set of tool designed for building interactive web applications.
+#project- Learning Log (an online journal system that lets you keep track of information you've learned about particular topics.)
+#writing spec
+#spec / specification - a document that explains how a project should be completed.
+
+#creating a virtual environment
+#A virtual environment is a place on your system where you can install packages for a particular project without affecting the system-wide installation of packages.
