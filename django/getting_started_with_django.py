be963d41727eca14db4e951594e2b137637ce814
calebcheptumo <ckibusia@gmail.com>
2024-02-08 21:42:26 +0300

    activating and deactivating virtual environment

--- django/getting_started_with_django.py
+++ django/getting_started_with_django.py
@@ -6,3 +6,7 @@
 
 #creating a virtual environment
 #A virtual environment is a place on your system where you can install packages for a particular project without affecting the system-wide installation of packages.
+# Command python -m venv (name of the virtual environment)
+#activate the virtual environment
+# Command source name_of_virtual_environment/bin/activate - this command runs the script activate in the bin directory of the virtual environment.
+# to stop the virtual environment, use the command deactivate
