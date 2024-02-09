602428dcbc773b77ee4fa5bab778bf2b98cf3594
calebcheptumo <ckibusia@gmail.com>
2024-02-09 11:13:33 +0300

    Registering Topic model with admin site

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,3 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin #importing the admin module from django.contrib
+from .models import Topic #importing the Topic model from the models module in the same directory
 
-# Register your models here.
+admin.site.register(Topic) #registering the Topic model with the admin site. manage our model through the admin site.
