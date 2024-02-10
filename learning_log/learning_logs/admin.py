b9850089b78b33c0bcffc1dc673839df1eb2bac2
calebcheptumo <ckibusia@gmail.com>
2024-02-10 21:13:11 +0300

    Registering Entry  model with admin site

--- learning_log/learning_logs/admin.py
+++ learning_log/learning_logs/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin #importing the admin module from django.contrib
-from .models import Topic #importing the Topic model from the models module in the same directory
+from .models import Topic , Entry #importing the Topic model from the models module in the same directory
 
 admin.site.register(Topic) #registering the Topic model with the admin site. manage our model through the admin site.
+admin.site.register(Entry) #registering the Entry model with the admin site. manage our model through the admin site.
