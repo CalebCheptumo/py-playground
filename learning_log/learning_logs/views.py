123442a780c71388eef7cf0d2fe542223fdd8d47
calebcheptumo <ckibusia@gmail.com>
2024-02-10 22:41:34 +0300

    Learning_log views

--- learning_log/learning_logs/views.py
+++ learning_log/learning_logs/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render #import the render() function from django.shortcuts module which renders the response based on the data provided by views
 
-# Create your views here.
+
+def index(request): #define a function called index that takes in a request object. the request object is required by django for all views.
+    """The homepage for learning log"""
+    return render(request, 'learning_log/index.html') #call the render() function with two arguments: the original request object and a template it can use to build the page. 
